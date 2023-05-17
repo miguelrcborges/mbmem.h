e21725ca7882e36b49cba323bf291c02bdcd928e
Miguel Borges <up202004481@edu.fe.up.pt>
2023-05-17 20:35:10 +0100

    some progress

--- mbnn.h
+++ mbnn.h
@@ -1,5 +1,5 @@
-#ifndef NN_H
-#define NN_H
+#ifndef MBNN_H
+#define MBNN_H
 
 #include <immintrin.h>
 
@@ -13,9 +13,10 @@ typedef struct NNLayer NNLayer;
 typedef struct NNOutput NNOutput;
 typedef union GenericTarget GenericTarget;
 
-MBNN_DEF inline float _mm256_reduce_add_ps(__m256 x);
-MBNN_DEF NNLayer *NNLayer_create(unsigned int size, float weights[], float bias[]);
-MBNN_DEF void NNConnect(NNLayer *base, GenericTarget target);
+static inline float _mm256_reduce_add_ps(__m256 x);
+MBNN_DEF NNLayer *NN_Layer_create(unsigned int size);
+MBNN_DEF NNOutput *NN_Output_create(unsigned int size);
+MBNN_DEF void NN_Connect(NNLayer *base, GenericTarget target);
 
 union GenericTarget {
 	NNLayer *nnlayer;
@@ -24,19 +25,21 @@ union GenericTarget {
 
 struct NNLayer {
 	unsigned int nNeurons;
+	unsigned int isOutput;
 	__m256 *neurons;
 	__m256 *weights;
-	__m256 *bias;
+	float *bias;
 	GenericTarget target;
 };
 
 struct NNOutput {
-	unsigned nNeurons;
+	unsigned int nNeurons;
+	unsigned int isOutput;
 	__m256 *neurons;
 };
 
 
-#ifdef NN_H_IMPLEMENTATIONS
+#ifdef MBNN_H_IMPLEMENTATIONS
 
 inline float _mm256_reduce_add_ps(__m256 x) {
     const __m128 x128 = _mm_add_ps(_mm256_extractf128_ps(x, 1), _mm256_castps256_ps128(x));
@@ -45,22 +48,55 @@ inline float _mm256_reduce_add_ps(__m256 x) {
     return _mm_cvtss_f32(x32);
 }
 
-
-NNLayer *NNLayer_create(unsigned int size, float weights[], float bias[]) {
-	NNLayer *l = (NNLayer *)malloc(sizeof(NNLayer));
-	size += 8 - size % 8;
-	l->neurons = (__m256 *)malloc(size / 8);
+NNLayer *NN_Layer_create(unsigned int size) {
+	NNLayer *l = (NNLayer *)calloc(1, sizeof(NNLayer));
+	if (size % 8 != 0) {
+		size += 8 - size % 8;
+	}
+	l->neurons = (__m256 *)calloc(size / 8, sizeof(__m256));
 	l->nNeurons = size;
+	l->isOutput = 0;
 	return l;
 }
 
-void NNConnect(NNLayer *base, GenericTarget target) {
-	base->target = target;
-	unsigned int n = sizeof(__m256) * (target.nnoutput->nNeurons / 8 + (target.nnoutput->nNeurons % 8 != 0));
-	base->weights = (__m256 *)malloc(n);
-	base->bias = (__m256 *)malloc(n);
+NNOutput *NN_Output_create(unsigned int size) {
+	NNOutput *o = (NNOutput *)calloc(1, sizeof(NNLayer));
+	if (size % 8 != 0) {
+		size += 8 - size % 8;
+	}
+	o->neurons = (__m256 *)calloc(size / 8, sizeof(__m256));
+	o->nNeurons = size;
+	o->isOutput = 1;
+	return o;
 }
 
-#endif /* NN_H_IMPLEMENTATIONS */
+void NN_Connect(NNLayer *base, GenericTarget target) {
+	base->target = target;
+	unsigned int n_weights = base->nNeurons * target.nnoutput->nNeurons;
+	base->weights = (__m256 *)calloc(n_weights / 8, sizeof(__m256));
+	base->bias = (float *)calloc(target.nnoutput->nNeurons, sizeof(float));
+}
 
-#endif /* NN_H */
+void NN_Initialize_Neurons(NNLayer *base, float *values, unsigned int nElems) {
+	for (size_t i = 0; i < nElems; i += 1) {
+		base->neurons[i / 8][i % 8] = values[i];
+	}
+}
+
+void NN_Compute(NNLayer *l) {
+	const int avx_ops_per_neuron = l->nNeurons / 8;
+	while (!l->isOutput) {
+		for (size_t i = 0; i < l->target.nnlayer->nNeurons; ++i) {
+			float result = l->bias[i];
+			for (size_t ii = 0; ii < avx_ops_per_neuron; ii += 8) {
+				result += _mm256_reduce_add_ps(_mm256_mul_ps(l->neurons[ii], l->weights[avx_ops_per_neuron * i + ii]));
+			}
+			l->target.nnlayer->neurons[i / 8][i % 8] = result;
+		}
+		l = l->target.nnlayer;
+	}
+}
+
+#endif /* MBNN_H_IMPLEMENTATIONS */
+
+#endif /* MBNN_H */
